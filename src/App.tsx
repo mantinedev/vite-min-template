b39544a7d643ea0638eef1cf9ac61eda91ef49cd
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-14 13:24:17 +0400

    Add theme and meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
 
 export default function App() {
-  return <MantineProvider>App</MantineProvider>;
+  return <MantineProvider theme={theme}>App</MantineProvider>;
 }
